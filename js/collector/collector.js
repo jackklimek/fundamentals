e341b70c0d7a112263cb3bd8f03c558d36eaf994
Jack Klimek <Jackklimek@Jacks-MacBook-Pro.local>
2018-05-15 14:08:04 -0400

    Programming fundamentals

---
+++ js/collector/collector.js
@@ -0,0 +1,16 @@
+let peoplewhoannoyme = [ 'Fred from Youtube', 'Trump', 'Nicolas Cage', 'Liam Neeson' ];
+peoplewhoannoyme.push('my roommates girlfriend');
+
+let last = peoplewhoannoyme.pop();
+
+console.log(last);
+
+let first = peoplewhoannoyme.shift();
+
+console.log(first);
+
+console.log(peoplewhoannoyme);
+
+peoplewhoannoyme.sort();
+
+console.log(peoplewhoannoyme);	
